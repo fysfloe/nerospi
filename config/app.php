72ded5513fa41e2f8318396e51952ca28dd222ef
Florian Csizmazia <florian.csizmazia@gmail.com>
2017-08-26 11:47:54 +0200

    Add Heroku creds and update default route to hit the DB.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Nerospi'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://nerospi.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
